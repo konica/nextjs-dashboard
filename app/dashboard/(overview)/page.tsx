54e8f284a32983f5a76a0f4d70ba7681d8bae24b
konica <dtrungthao@outlook.com>
2024-10-26 06:27:35 +0700

    Practice search & pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,11 @@
 import { Suspense } from 'react';
-// import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 
-// import { fetchCardData } from '@/app/lib/data';
-
 export default async function Page() {
-    // const { totalPendingInvoices, totalPaidInvoices, numberOfCustomers, numberOfInvoices } = await fetchCardData();
 
     return (
         <main>
@@ -17,14 +13,6 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                <Card title="Pending" value={totalPendingInvoices} type="pending" />
-                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-                <Card
-                    title="Total Customers"
-                    value={numberOfCustomers}
-                    type="customers"
-                /> */}
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper></CardWrapper>
                 </Suspense>
